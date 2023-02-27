615cd7202c5502fc8dc5c31e2334369ce806ec9a
Zhiyi Tang <zoe01@berkeley.edu>
2023-02-26 20:50:18 -0800

    hw2 version4

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private WeightedQuickUnionUF square;
+    private WeightedQuickUnionUF square2;
     private boolean[][] id;
     private int rowLength;
     private int size;
@@ -17,6 +18,7 @@ public class Percolation {
             }
         }
         square = new WeightedQuickUnionUF(N * N + 2);
+        square2 = new WeightedQuickUnionUF(N * N + 1);
         rowLength = N;
         size = 0;
     }
@@ -30,19 +32,24 @@ public class Percolation {
             size += 1;
             if (row - 1 >= 0 && id[row - 1][col]) {
                 square.union(xyTo1D(row - 1, col), xyTo1D(row, col));
+                square2.union(xyTo1D(row - 1, col), xyTo1D(row, col));
             }
             if (col - 1 >= 0 && id[row][col - 1]) {
                 square.union(xyTo1D(row, col - 1), xyTo1D(row, col));
+                square2.union(xyTo1D(row, col - 1), xyTo1D(row, col));
             }
             if (col + 1 < rowLength && id[row][col + 1]) {
                 square.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+                square2.union(xyTo1D(row, col), xyTo1D(row, col + 1));
             }
             if (row + 1 < rowLength && id[row + 1][col]) {
                 square.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+                square2.union(xyTo1D(row, col), xyTo1D(row + 1, col));
             }
 
             if (row == 0) {
                 square.union(xyTo1D(row, col), rowLength * rowLength);
+                square2.union(xyTo1D(row, col), rowLength * rowLength);
             }
             if (row == rowLength - 1) {
                 if (!this.percolates()) {
@@ -64,7 +71,7 @@ public class Percolation {
     }
 
     public boolean isFull(int row, int col) {
-        if (square.connected(xyTo1D(row, col), rowLength * rowLength)) {
+        if (square2.connected(xyTo1D(row, col), rowLength * rowLength)) {
             return true;
         }
         return false;
